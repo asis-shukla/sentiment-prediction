aafa60ee38be2ced43603b36adfc84d0b8dc0870
Ashish Shukla <ashishfun008@gmail.com>
2020-10-18 00:11:15 +0530

    react app created using create-react-app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Sentiment Prediction App for English Text</h1>
+      <h2> Under Development | Thanks for visit</h2>
+    </div>
+  );
+}
+
+export default App;
